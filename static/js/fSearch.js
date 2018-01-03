2c1a54aacfa842c1a4a4b238bb2b9b659154a8bb
Ian Kennedy <ian@resolutionathens.com>
2018-01-03 09:45:49 -0500

    adding type-ahead to activity search

--- static/js/fSearch.js
+++ static/js/fSearch.js
@@ -1,5 +1,5 @@
 // import fetch from 'node-fetch';
-const endpoint = './activities.js';
+const endpoint = '/js/activities-scratch.js';
 
 const activities = [];
 fetch(endpoint)
@@ -20,3 +20,32 @@ function initSearch() {
   console.log('initSearch fired and all else was loaded.')
 }
 
+function findMatches(wordToMatch, activities) {
+  return activities.filter(activity => {
+    const regex = new RegExp(wordToMatch, 'gi');
+    return activity.name.match(regex)
+  });
+}
+
+function displayMatches() {
+  const matchArray = findMatches(this.value, activities).slice(0,5);
+  const html = matchArray.map(activity => {
+    const regex = new RegExp(this.value, 'gi');
+    const activityName = activity.name.replace(regex, `<span class="hl">${this.value}</span>`);
+    return `
+      <li>
+        <span class="name">${activityName}</span>
+      </li>
+    `;
+  }).join('');
+  suggestions.innerHTML = html;
+  if (this.value === '') {
+    suggestions.innerHTML = '';
+  }
+}
+
+const searchInput = document.querySelector('#fActivity');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
